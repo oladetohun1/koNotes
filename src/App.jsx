73d42689b9e0d82ea93ce4bff26307ebcdad79d3
latiniuxs <koredeoladetohun@gmail.com>
2023-08-23 23:10:19 +0100

    .

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import {nanoid} from 'nanoid';
+import NotesList from "./components/NotesLists";
+
+const App = () => {
+  const {notes, setNotes} = useState([
+    {
+      id: nanoid(),
+      text:"This is my first note",
+      date:"22/08/2023",
+    },
+    {
+      id: nanoid(),
+      text:"This is my second note",
+      date:"22/08/2023",
+    },
+    {
+      id: nanoid(),
+      text:"This is my third note",
+      date:"22/08/2023",
+    },
+  ]);
+  
+  return (
+    <div className="container">
+      {<NotesList  notes = {notes}/>}
+    </div>
+
+  );
+}
+
+export default App;
